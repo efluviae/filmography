027780f02ae53f38e27d98c5856181798b1a9ec3
Eden <edenpolarismseifert@gmail.com>
2026-01-29 09:25:15 -0300

    initialize git

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import NavBar from "./components/NavBar";
+import { MovieProvider } from "./context/MovieContext";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
+  );
+}
+
+export default App;
